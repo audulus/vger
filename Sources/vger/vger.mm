ee57efdecd26358ca973afc4be1ff455f7334ac9
Taylor Holliday <taylor@audulus.com>
2021-04-23 11:29:56 -0700

    Update vger.mm

--- Sources/vger/vger.mm
+++ Sources/vger/vger.mm
@@ -73,7 +73,7 @@ void vgerRenderText(vger* vg, const char* str, float4 color) {
         vgerPrim prim = {
             .type = vgerRect,
            // .texture = -info.regionIndex,
-            .cvs = {p, float2{float(info.glyphSize.width), float(info.glyphSize.height)}},
+            .cvs = {p, p+float2{float(info.glyphSize.width), float(info.glyphSize.height)}},
             .xform=matrix_identity_float3x3,
             .width = 0.01,
             .radius = 0,
