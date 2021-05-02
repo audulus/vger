f8907330ab3a4eb608c4c8f9e48f2ff38fc28dce
Taylor Holliday <taylor@audulus.com>
2021-05-01 17:43:58 -0700

    add vgerAlign

--- Sources/vger/include/vger_types.h
+++ Sources/vger/include/vger_types.h
@@ -84,4 +84,16 @@ typedef struct {
 
 } vgerPrim;
 
+enum vgerAlign {
+    // Horizontal align
+    VGER_ALIGN_LEFT      = 1<<0, // Default, align text horizontally to left.
+    VGER_ALIGN_CENTER    = 1<<1, // Align text horizontally to center.
+    VGER_ALIGN_RIGHT     = 1<<2, // Align text horizontally to right.
+    // Vertical align
+    VGER_ALIGN_TOP       = 1<<3, // Align text vertically to top.
+    VGER_ALIGN_MIDDLE    = 1<<4, // Align text vertically to middle.
+    VGER_ALIGN_BOTTOM    = 1<<5, // Align text vertically to bottom.
+    VGER_ALIGN_BASELINE  = 1<<6, // Default, align text vertically to baseline.
+};
+
 #endif /* vger_types_h */
