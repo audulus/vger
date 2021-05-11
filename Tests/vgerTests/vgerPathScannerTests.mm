616c577cc006eddf4fd2f0b6f8664881c70cf314
Taylor Holliday <taylor@audulus.com>
2021-05-11 06:53:07 -0700

    add test

--- Tests/vgerTests/vgerPathScannerTests.mm
+++ Tests/vgerTests/vgerPathScannerTests.mm
@@ -38,4 +38,33 @@
     printf("done\n");
 }
 
+- (void)testScanCGPath {
+ 
+    auto path = CGPathCreateMutable();
+    
+    vector_float2 cvs[] = { {1,0}, {1,1}, {0,1}, {-1, 1}, {-1,0}, {-1, -1}, {0,-1}, {1,-1}, {1,0}};
+    
+    int n = sizeof(cvs)/sizeof(vector_float2);
+    CGPathMoveToPoint(path, nullptr, cvs[0].x, cvs[0].y);
+    for(int i=1;i<n-1;i+=2) {
+        CGPathAddQuadCurveToPoint(path, nullptr, cvs[i].x, cvs[i].y, cvs[i+1].x, cvs[i+1].y);
+    }
+    CGPathCloseSubpath(path);
+    
+    vgerPathScanner scan;
+    scan.begin(path);
+    
+    while(scan.next()) {
+        printf("interval %f %f, active: ", scan.yInterval.a, scan.yInterval.b);
+        for(int a = scan.first; a != -1; a = scan.segments[a].next) {
+            printf("%d ", a);
+        }
+        printf("\n");
+    }
+
+    printf("done\n");
+    
+    CGPathRelease(path);
+}
+
 @end
