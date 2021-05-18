6a7732de7fcb656324d9524aebba5b8824af5eed
Taylor Holliday <taylor@audulus.com>
2021-05-18 12:07:20 -0700

    increase tile buffer size

--- Sources/vger/commands.h
+++ Sources/vger/commands.h
@@ -18,7 +18,7 @@ using namespace simd;
 // Rendering commands for experimental tile-based fine rendering.
 // Not yet in use.
 
-#define tileBufSize 64
+#define tileBufSize 256
 #define maxTilesWidth 256
 #define tileSize 16
 
