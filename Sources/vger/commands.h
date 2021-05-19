712167fe687ce4d2021567b283fc7e7277d719ce
Taylor Holliday <taylor@audulus.com>
2021-05-18 17:24:45 -0700

    Update commands.h

--- Sources/vger/commands.h
+++ Sources/vger/commands.h
@@ -20,7 +20,7 @@ using namespace simd;
 // Rendering commands for experimental tile-based fine rendering.
 // Not yet in use.
 
-#define tileBufSize 256
+#define tileBufSize 1024
 #define maxTilesWidth 256
 #define tileSize 16
 
