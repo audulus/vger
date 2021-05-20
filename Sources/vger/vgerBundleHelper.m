dab9c926dae950347ec533f210afcc7523e5a75e
Taylor Holliday <taylor@audulus.com>
2021-05-20 13:55:01 -0700

    Update vgerBundleHelper.m

--- Sources/vger/vgerBundleHelper.m
+++ Sources/vger/vgerBundleHelper.m
@@ -5,7 +5,11 @@
 @implementation vgerBundleHelper
 
 + (NSBundle*) moduleBundle {
+#ifdef SWIFTPM_MODULE_BUNDLE
     NSBundle* bundle = SWIFTPM_MODULE_BUNDLE;
+#else
+    NSBundle* bundle = [NSBundle mainBundle];
+#endif
     assert(bundle);
     return bundle;
 }
