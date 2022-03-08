80586d63dba3ba707fd4665d08962a2a7fd787bb
Aurelius Prochazka <aurephd@gmail.com>
2022-03-07 20:51:29 -0800

    Started playground (not working for some reason)

---
+++ Playgrounds/Hello World.playground/Contents.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+import vger      // C/C++/ObjC interface.
+import vgerSwift // Swift niceties.
+
+struct HelloView: View {
+
+    let cyan = SIMD4<Float>(0,1,1,1)
+
+    var body: some View {
+        VgerView(renderCallback: { vger in
+            vgerText(vger, "Hello world. This is V'Ger.", cyan, 0)
+        })
+    }
+}
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+PlaygroundPage.current.setLiveView(HelloView())
