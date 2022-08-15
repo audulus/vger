ac676d1638887f58c9ce1a550abdc825c7ec7aa1
Taylor Holliday <taylor@audulus.com>
2022-08-15 16:43:30 -0700

    Add GPUVec

--- Sources/vger/vgerScene.h
+++ Sources/vger/vgerScene.h
@@ -6,6 +6,40 @@
 #import <Metal/Metal.h>
 #define VGER_MAX_LAYERS 4
 
+template<class T>
+struct GPUVec {
+    id<MTLBuffer> buffer;
+    T* ptr;
+    size_t count = 0;
+    size_t capacity = 65536;
+
+    GPUVec(id<MTLDevice> device) {
+        buffer = [device newBufferWithLength:capacity * sizeof(T)
+                                     options:MTLResourceStorageModeShared];
+        ptr = buffer.contents;
+    }
+
+    void allocate(id<MTLDevice> device, size_t cap) {
+        capacity = cap;
+        buffer = [device newBufferWithLength:capacity * sizeof(T)
+                                     options:MTLResourceStorageModeShared];
+        memcpy(buffer.contents, ptr, count * sizeof(T));
+        ptr = buffer.contents;
+    }
+
+    void append(const T& value) {
+
+        if(count >= capacity) {
+            allocate(buffer.device, capacity*2);
+        }
+        ptr[count++] = value;
+    }
+
+    void reset() {
+        count = 0;
+    }
+};
+
 struct vgerScene {
     id<MTLBuffer> prims[VGER_MAX_LAYERS];  // vgerPrim
     id<MTLBuffer> cvs;    // float2
