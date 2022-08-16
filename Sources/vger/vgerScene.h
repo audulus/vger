768d1142e7d65e493d0bba815318c331b469e20e
Taylor Holliday <taylor@audulus.com>
2022-08-15 17:06:19 -0700

    Fix casts

--- Sources/vger/vgerScene.h
+++ Sources/vger/vgerScene.h
@@ -20,7 +20,7 @@ struct GPUVec {
     GPUVec(id<MTLDevice> device) {
         buffer = [device newBufferWithLength:capacity * sizeof(T)
                                      options:MTLResourceStorageModeShared];
-        ptr = static_cast<vgerPrim*>(buffer.contents);
+        ptr = static_cast<T*>(buffer.contents);
     }
 
     void allocate(id<MTLDevice> device, size_t cap) {
@@ -28,7 +28,7 @@ struct GPUVec {
         buffer = [device newBufferWithLength:capacity * sizeof(T)
                                      options:MTLResourceStorageModeShared];
         memcpy(buffer.contents, ptr, count * sizeof(T));
-        ptr = static_cast<vgerPrim*>(buffer.contents);
+        ptr = static_cast<T*>(buffer.contents);
     }
 
     void append(const T& value) {
