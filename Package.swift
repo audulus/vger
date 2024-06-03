a627d4cc9d2d170b642c3efc7fed8999ef185414
Taylor Holliday <wtholliday@gmail.com>
2024-06-03 10:38:09 -0700

    update swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 
 import PackageDescription
 
@@ -6,7 +6,7 @@ let package = Package(
     name: "vger",
     platforms: [.macOS(.v11), .iOS(.v14)],
     products: [.library(name: "vger", targets: ["vger", "vgerSwift"])],
-    dependencies: [.package(url: "https://github.com/wtholliday/MetalNanoVG", Package.Dependency.Requirement.branch("spm"))],
+    dependencies: [.package(url: "https://github.com/wtholliday/MetalNanoVG", branch: "spm")],
     targets: [
         .target(name: "vger", dependencies: [], resources: [.copy("fonts")]),
         .target(name: "vgerSwift", dependencies: ["vger"]),
