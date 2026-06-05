a4875fc8b4c0558df75620c771c45ec80b61549c
Taylor Holliday <wtholliday@gmail.com>
2026-06-04 19:23:32 -0700

    Compare test textures by pixel diff

--- Tests/vgerTests/testUtils.mm
+++ Tests/vgerTests/testUtils.mm
@@ -1,9 +1,13 @@
 //  Copyright © 2021 Audulus LLC. All rights reserved.
 
 #include "testUtils.h"
+#include <algorithm>
+#include <cstdlib>
 #include <vector>
 #import <ImageIO/ImageIO.h>
 #import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
+
+static constexpr int kMaxPixelDifference = 2;
 
 void writeCGImage(CGImageRef image, CFURLRef url) {
     auto dest = CGImageDestinationCreateWithURL(url, (CFStringRef) UTTypePNG.identifier, 1, nil);
@@ -102,13 +106,53 @@ void showTexture(id<MTLTexture> texture, NSString* name) {
 
 @end
 
+static CGImageRef createImage(NSURL* url) {
+    auto source = CGImageSourceCreateWithURL((__bridge CFURLRef)url, nil);
+    if(!source) {
+        return nil;
+    }
+
+    auto image = CGImageSourceCreateImageAtIndex(source, 0, nil);
+    CFRelease(source);
+    return image;
+}
+
+static bool getImagePixels(CGImageRef image, std::vector<UInt8>& pixels) {
+    if(!image) {
+        return false;
+    }
+
+    auto w = CGImageGetWidth(image);
+    auto h = CGImageGetHeight(image);
+    pixels.assign(4*w*h, 0);
+
+    auto colorSpace = CGColorSpaceCreateDeviceRGB();
+    auto bitmapInfo = static_cast<CGBitmapInfo>(kCGBitmapByteOrder32Big) |
+                      static_cast<CGBitmapInfo>(kCGImageAlphaPremultipliedLast);
+    auto context = CGBitmapContextCreate(pixels.data(),
+                                         w,
+                                         h,
+                                         8,
+                                         4*w,
+                                         colorSpace,
+                                         bitmapInfo);
+    CGColorSpaceRelease(colorSpace);
+
+    if(!context) {
+        return false;
+    }
+
+    CGContextDrawImage(context, CGRectMake(0, 0, w, h), image);
+    CGContextRelease(context);
+    return true;
+}
+
 bool checkTexture(id<MTLTexture> texture, NSString* name) {
 
     auto tmpURL = [NSFileManager.defaultManager.temporaryDirectory URLByAppendingPathComponent:name];
     NSLog(@"saving to %@", tmpURL);
     CGImageRef image = createImage(texture);
     writeCGImage(image, (__bridge CFURLRef)tmpURL);
-    CGImageRelease(image);
 
     // Get URL for baseline.
     NSString* path = @"Contents/Resources/vger_vgerTests.bundle/Contents/Resources/images/";
@@ -117,13 +161,59 @@ bool checkTexture(id<MTLTexture> texture, NSString* name) {
     auto baselineURL = [bundle.bundleURL URLByAppendingPathComponent:path];
     NSLog(@"checking against baseline %@", baselineURL);
 
-    bool equal = [NSFileManager.defaultManager contentsEqualAtPath:baselineURL.path andPath:tmpURL.path];
+    CGImageRef baselineImage = createImage(baselineURL);
+    bool equal = false;
+
+    if(!baselineImage) {
+        NSLog(@"missing or unreadable baseline image %@", baselineURL);
+    } else if(CGImageGetWidth(image) != CGImageGetWidth(baselineImage) ||
+              CGImageGetHeight(image) != CGImageGetHeight(baselineImage)) {
+        NSLog(@"image size mismatch: got %zux%zu, expected %zux%zu",
+              CGImageGetWidth(image),
+              CGImageGetHeight(image),
+              CGImageGetWidth(baselineImage),
+              CGImageGetHeight(baselineImage));
+    } else {
+        std::vector<UInt8> actualPixels;
+        std::vector<UInt8> baselinePixels;
+
+        if(getImagePixels(image, actualPixels) && getImagePixels(baselineImage, baselinePixels)) {
+            int maxDifference = 0;
+            size_t maxDifferenceIndex = 0;
+
+            for(size_t i=0;i<actualPixels.size();++i) {
+                int difference = std::abs(int(actualPixels[i]) - int(baselinePixels[i]));
+                if(difference > maxDifference) {
+                    maxDifference = difference;
+                    maxDifferenceIndex = i;
+                }
+            }
+
+            equal = maxDifference <= kMaxPixelDifference;
+            if(!equal) {
+                auto pixelIndex = maxDifferenceIndex / 4;
+                NSLog(@"maximum pixel difference %d exceeds epsilon %d at (%zu, %zu), channel %zu",
+                      maxDifference,
+                      kMaxPixelDifference,
+                      pixelIndex % CGImageGetWidth(image),
+                      pixelIndex / CGImageGetWidth(image),
+                      maxDifferenceIndex % 4);
+            }
+        } else {
+            NSLog(@"could not decode images for comparison");
+        }
+    }
 
     if(!equal) {
 #if TARGET_OS_OSX
         system([NSString stringWithFormat:@"open %@", tmpURL.path].UTF8String);
 #endif
     }
+
+    if(baselineImage) {
+        CGImageRelease(baselineImage);
+    }
+    CGImageRelease(image);
 
     return equal;
 }
